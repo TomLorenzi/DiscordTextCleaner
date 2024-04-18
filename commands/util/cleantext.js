d34f41abeba54c6ab12849391b22a27cc2750ea4
Thomas De Lorenzi <tdelorenzi@interactiv.tech>
2024-04-18 15:17:19 +0200

    improve cleantext

--- commands/util/cleantext.js
+++ commands/util/cleantext.js
@@ -7,12 +7,46 @@ module.exports = {
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 	async execute(interaction) {
         const channel = interaction.channel;
-        const messages = await channel.messages.fetch({ limit: 100 });
-        messages.forEach(async message => {
+        const messages = await fetchMore(channel, 1000);
+        for (const message of messages.values()) {
             if (message.attachments.size === 0 && !message.content.includes('http')) {
                 await message.delete();
             }
-        });
+        }
 		await interaction.reply({content: 'Le texte a été nettoyé !', ephemeral: true});
 	},
 };
+
+async function fetchMore(channel, limit = 250) {
+    if (!channel) {
+        throw new Error(`Expected channel, got ${typeof channel}.`);
+    }
+    if (limit <= 100) {
+        return channel.messages.fetch({ limit });
+    }
+
+    let collection = new Collection();
+    let lastId = null;
+    let options = {};
+    let remaining = limit;
+
+    while (remaining > 0) {
+        options.limit = remaining > 100 ? 100 : remaining;
+        remaining = remaining > 100 ? remaining - 100 : 0;
+
+        if (lastId) {
+        options.before = lastId;
+        }
+
+        let messages = await channel.messages.fetch(options);
+
+        if (!messages.last()) {
+        break;
+        }
+
+        collection = collection.concat(messages);
+        lastId = messages.last().id;
+    }
+
+    return collection;
+}
